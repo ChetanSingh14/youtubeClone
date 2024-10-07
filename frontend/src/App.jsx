92a1fcd1ec0ce142d669e5de34cf148dc86bfaf2
ChetanSingh14 <siradhanachetan14@gmail.com>
2024-10-07 17:28:01 +0530

    Filter Container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SideBar from './component/SideBar'
+import Filter from './Component/Filter';
 
 export default function App() {
   return (
     <div className='app'>
          <SideBar/>
+         <Filter/>
     </div>
   )
 }
