cc7c30e8d26cf8e07fc0e0daf4314e46cb0b738f
Arihant-Singh-Rana <arihantsinghranafst@gmail.com>
2024-10-08 23:47:19 +0530

    Avatar stuff (Not completed)

--- frontend/src/Component/authorization/SignInandAvatar.jsx
+++ frontend/src/Component/authorization/SignInandAvatar.jsx
@@ -2,20 +2,42 @@ import React from 'react';
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Style from "./SignInandAvatar.module.css";
 import {useNavigate} from 'react-router-dom';
-import avatar from '../../assets/people.png'
-
-export default function SignInandLogOut({SignedIn = false}) {
+import avatar from '../assets/people.png'
+import GoogleIcon from "@mui/icons-material/Google";
+import SwitchAccountIcon from "@mui/icons-material/SwitchAccount";
+import LogoutIcon from "@mui/icons-material/Logout";
+//This Component is called in Navbar.jsx
+export default function SignInandLogOut({SignedIn = true}) {
   const nav = useNavigate();
   return (
     <div className={Style.ButtonParentContainer}>
       {SignedIn ? (
         <div className={Style.AvatarContainer}>
-          <img src={avatar} alt="Avatar" className={Style.Avatar}/>
+          <img src={avatar} alt="Avatar" className={Style.Avatar} />
+          <div className={Style.AvatarMenu}>
+            <div className={Style.UserDesc}>
+              <img src={avatar} alt="Avatar" className={Style.Avatar} />
+              <div className={Style.UserDescText}>
+                Hi
+                Arihant
+              </div>
+            </div>
+            <ul>
+              <li><GoogleIcon/> Google Account</li>
+              <li><SwitchAccountIcon/> Switch Accounts</li>
+              <li><LogoutIcon/> Sign Out</li>
+            </ul>
+          </div>
         </div>
       ) : (
         <div className={Style.buttonContainer}>
-          <button className={Style.SignInButton} onClick={() =>{nav('/auth')}}>
-            <AccountCircleIcon fontSize='small'/> 
+          <button
+            className={Style.SignInButton}
+            onClick={() => {
+              nav("/auth");
+            }}
+          >
+            <AccountCircleIcon fontSize="small" />
             Sign In
           </button>
         </div>
